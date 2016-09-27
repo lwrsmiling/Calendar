c5b7a47c4cbe8dfdea94de721a907605e532af76
lwrsmiling <wanru@ualberta.ca>
2016-09-26 22:43:01 -0600

    last year

--- My Calendar.js
+++ My Calendar.js
@@ -10,3 +10,11 @@ for(var i = 0; i <= year_options.length; i++){
   var y_option= year_options[i];
   y_selected.options.add(new Option(y_option.text,y_option.value,y_option.selected));
 }
+
+function lastYear(){
+  var current_year= y_selected.options[y_selected.selectedIndex].value;
+  var updated_year= current_year-1;
+  var updated_yeartxt= updated_year.toString();
+  y_selected.options[y_selected.selectedIndex].value = updated_year;
+  y_selected.options[y_selected.selectedIndex].text = updated_yeartxt;
+}
