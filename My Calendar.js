bc5a98a69a5261178c4d11ac2b2b1103f1e8bdf7
lwrsmiling <wanru@ualberta.ca>
2016-09-28 22:44:24 -0600

    dateTable

--- My Calendar.js
+++ My Calendar.js
@@ -25,3 +25,21 @@ function nextYear(){
   y_selected.options[y_selected.selectedIndex].value = updated_year;
   y_selected.options[y_selected.selectedIndex].text = updated_year.toString();
 }
+
+/* create a datetable*/
+
+function generateDateTable(){
+  var dateTable = document.getElementById("dateTable");
+  var rows = 5;
+  var columns = 6;
+  var
+    tds = "<td>".repeat(columns),
+    trs = ('<tr>'+tds).repeat(rows),
+    table = "<table>" + trs + "</table>";
+
+  dateTable.innerHTML= table;
+  String.prototype.repeat = function( num ){
+    return new Array( num + 1 ).join( this );
+  }
+  
+}
